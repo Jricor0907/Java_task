ab63ee3cbd1e020e9be0eaeb9c2ce32c2a430659
Juan Rico <juanricor23@gmail.com>
2021-10-20 14:00:36 -0700

    Fist

---
+++ Lab3Q3.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Lab3Q3 {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> make = new ArrayList<>();
+        make.add("Toyota");
+        make.add("Honda");
+        make.add("Nissan");
+
+        ArrayList<String> model = new ArrayList<>();
+        model.add("Type1");
+        model.add("Type2");
+        model.add("Type3");
+
+        ArrayList<Integer> year = new ArrayList<>();
+        year.add(1990);
+        year.add(1994);
+        year.add(1991);
+        Collections.sort(year);
+        System.out.println(year);
+
+    }
+
+}
